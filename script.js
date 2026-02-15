0e3fd18c48b96b4f929c2b64c8cf0f1335a82f27
swglennmark-ctrl <swglennmark@gmail.com>
2026-02-15 15:31:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ const gifStages = [
 
 const noMessages = [
     "No",
-    "Are you positive? 🤔",
-    "Pookie please... 🥺",
+    "hmmmmm? 🤔",
+    "Please... 🥺",
     "If you say no, I will be really sad...",
     "I will be very sad... 😢",
     "Please??? 💔",
